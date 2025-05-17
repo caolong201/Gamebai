fdc6ee6f389110025e2ceb2530ef386314744970
caolong201 <126745101+caolong201@users.noreply.github.com>
2025-05-17 18:34:55 +0700

    UpdateLoadLuat

--- Assets/Scripts/UI/Base/UIPopup/UIRulesBox.cs
+++ Assets/Scripts/UI/Base/UIPopup/UIRulesBox.cs
@@ -3,19 +3,21 @@ using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
+//using UnityEngine.UIElements;
 using System.Collections;
 
 public class UIRulesBox : GUIBaseDialogHandler
 {
-
+    [SerializeField] Image load;
     public float collapsedHeight = 200f;
     public float expandedHeight = 500f;
     public float animationDuration = 0.3f;
 
     private Coroutine currentAnim;
     public RectTransform scrollView;
+    
     private Action OnCloseClick;
+    [SerializeField] private GameObject[] uiElementsToShowAfterLoad;
 
     public override void OnBeginShow(object parameter)
     {
@@ -24,8 +26,35 @@ public class UIRulesBox : GUIBaseDialogHandler
         UIRulesBoxData data = (UIRulesBoxData)parameter;
  
         OnCloseClick = data.onCloseClick;
+
+        SetUIElementsActive(false);
+        StartCoroutine(RotateLoadThenShowUI());
+    }
+    private void SetUIElementsActive(bool active)
+    {
+        foreach (var go in uiElementsToShowAfterLoad)
+        {
+            if (go != null)
+                go.SetActive(active);
+        }
     }
 
+    private IEnumerator RotateLoadThenShowUI()
+    {
+        float duration = 1f;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            load.transform.Rotate(Vector3.forward, -360 * Time.deltaTime / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        load.transform.rotation = Quaternion.identity;       
+        load.gameObject.SetActive(false);
+        SetUIElementsActive(true);
+    }
     public void OnbtnCloseClicked()
     {
         OnCloseClick?.Invoke();
